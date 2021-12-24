c88705898663a62da38881f862b4b3cdb9595486
nicholashutter <nicholas.hutter@live.com>
2021-12-23 22:45:11 -0600

    finished other fields, added formattting

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import './App.css';
 
 function App() {
   return (  
-<form>
+<form onSubmit= {handleSubmit}>
+    
   <label>
-   First Name:
-    <input type="text" name="first name" />
+  First Name:
+  <input type="text" name="first name" />
   </label>
-  <input type="submit" value="Submit" />
+  <br></br>
 
   <label>
    Last Name:
-    <input type="text" name="last name" />
+  <input type="text" name="last name" />
+  </label>
+  <br></br>
+
+  <label>
+   Address:
+  <input type="text" name="Address" />
+  </label>
+  <br></br>
+
+  <label>
+   Birthdate (xx/xx/xxxx):
+  <input type="text" name="Birthdate" />
   </label>
   <input type="submit" value="Submit" />
+  <br></br>
 </form>
 
   );
+}
+function handleSubmit(e)
+{
+  console.log("you clicked submit");
 }
 
 export default App;
