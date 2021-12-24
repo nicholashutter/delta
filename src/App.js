e5630ef3067f0c71298a9fac0596e3bdc9cbbc51
nicholashutter <nicholas.hutter@live.com>
2021-12-23 19:03:06 -0600

    take user first and last name complete

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (  
+<form>
+  <label>
+   First Name:
+    <input type="text" name="first name" />
+  </label>
+  <input type="submit" value="Submit" />
+
+  <label>
+   Last Name:
+    <input type="text" name="last name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+
+  );
+}
+
+export default App;
